a680a90ee9bede2afb9a8872646dfb3f53574178
Luke O'Malley <Lukeomalley95@gmail.com>
2019-11-08 16:49:17 -0500

    implemented push and pop for doublyLinkedList

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -6,10 +6,64 @@ class Node {
   }
 }
 
-class DoubleLinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  print() {
+    const list = [];
+    let currNode = this.head;
+    while (currNode) {
+      list.push(currNode.value);
+      currNode = currNode.next;
+    }
+    return list;
+  }
+
+  push(value) {
+    // insert after tail
+    const node = new Node(value);
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  pop() {
+    // remove the tail and return it
+    if (this.length === 0) {
+      return null;
+    }
+
+    const removedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedNode.prev;
+      this.tail.next = null;
+      removedNode.prev = null;
+      return removedNode;
+    }
+    this.length -= 1;
+    return removedNode;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.push(6);
